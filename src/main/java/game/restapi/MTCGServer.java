cfaee811aa520a92a21c99acf664a3a1264c2c7e
nbrandtner-tgm <nbrandtner@gmx.at>
2024-10-16 15:11:10 +0200

    intermediate handin + uml

--- src/main/java/game/restapi/MTCGServer.java
+++ src/main/java/game/restapi/MTCGServer.java
@@ -1,11 +1,14 @@
 package game.restapi;
 
 
-import game.deck.Deck;
+
 import game.packs.Package;
 import game.user.User;
+/*
+import game.deck.Deck;
 import game.card.Card;
 import com.fasterxml.jackson.annotation.JsonProperty;
+*/
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
